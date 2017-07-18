5f447b54e2654cd92ff7b0d42bb717eecb277316
chois lu <chois_lu@outlook.com>
2017-07-18 21:57:41 +0800

    add item.py update cross

--- cross/cross.py
+++ cross/cross.py
@@ -1,65 +1,97 @@
 # -*- coding: utf-8 -*-
 
 """
-作者：kissf lu
-日期：2017/7/7
-说明：杭州项目，汇流点类模块
+======================================================================================================
+                                                     杭州HUB仿真项目
 
+                                    项目启动日期：2017年7月6日
+                                    项目启动标识：AIRPORT OF EZHOU'S PROJECT  -- HZ
+                                    ===========================================
+                                    代码创建日期：2017年7月6日
+                                    代码创建工程师：卢健
+                                    代码版本：1.0
+                                    版本更新日期：2017年7月6日
+                                    版本更新工程师：卢健
+
+                                    代码整体功能描述：汇流点机器类模块
+                                                      1、二入一出模型， 本次只需要一个入口一个出口；
+                                                      2、无延时处理过程；
+                                                      3、每个入口\出口无服务受限；
+=====================================================================================================
 """
-
-
-from simpy import PriorityItem
+from cross.items import Pipeline
+from cross.items import Package
 
 
 class Cross(object):
     """
     Cross obj:
     sim one machine that have more than one input ports and one out put port.
-    input_i wrapped in a dict: input_dic = {'x1_in1': queue, ..., 'x1_ini': queue}.
+    input_i wrapped in a dict: input_dic =
+    {
+    'x1_in1': queue, ...,
+    'x1_ini': queue}.
                   _ _ _ _ _ _ _
                  |             |
      input_1 - ->|             |
          .       |    Cross    |- ->output
      input_i - ->|             |
                  |_ _ _ _ _ _ _|
-
     """
-    def __init__(self, env, id_x, input_dic: dict=None, out_put=None):
+    def __init__(self,
+                 env,
+                 machine_id,
+                 equipment_id,
+                 input_pip_line=None,
+                 pipelines_dict=None,
+                 resource_dict=None):
         """
         init class self args:
         Args:
             env: A simpy.Environment instance.
-            id_x: Cross machine id.
-            input_dic: A simpy.PriorityStore which was put from ahead machine.
-            out_put: out .
-
+            machine_id: Cross machine id.
+            equipment_id: 预留参数
+            input_pip_line: A simpy.PriorityStore which was put from
+                            ahead machine.
+            pipelines_dict: pip line 字典
+            resource_dict: 资源查询字典
+        Raises:
+            RuntimeError: An error occurred when input_dic
+            not initialized before.
         """
         self.env = env
-        self.id_x = id_x
-        self.input_dic = input_dic
-        self.out_put = out_put
+        self.machine_id = machine_id
+        self.equipment_id = equipment_id
+        self.input_pip_line = input_pip_line
+        self.pipelines_dict = pipelines_dict
+        self.resource_dict = resource_dict
         self.process = self._get_package_queue()
 
     def _get_package_queue(self):
         """
+        获取输入队列功能单元
         """
-        if self.input_dic:
-            for queue_id, get_package_queue in self.input_dic.items():
-                self.env.process(self._get_packages(queue_id, get_package_queue))
+        if self.input_pip_line:
+            if isinstance(self.input_pip_line, Pipeline):
+                self.env.process(self._get_packages(self.input_pip_line))
         else:
-            raise RuntimeError('Please Initial input port Queue for Cross instance First!')
+            raise RuntimeError('Please Initial input pip line '
+                               'Queue for Cross instance First!')
 
-    def _get_packages(self, queue_id, get_package_queue):
+    def _get_packages(self, get_package_queue):
         """
         """
         while True:
-            yield self.env.timeout(10)
-            print('len of package_queue', queue_id , 'is', len(get_package_queue.items), 'at', self.env.now)
             packages = yield get_package_queue.get()
-            print(f"------->package {packages.item['package_id']}", 'was push to next queue at', self.env.now)
-            self.env.process(self._put_packages_into_out_queue(packages))
+            # 判断取没取到货物
+            if packages:
+                print(f"------->package {packages.item['package_id']}",
+                      'was push to next queue at', self.env.now)
+                self.env.process(self._put_packages_into_out_queue(packages))
 
     def _put_packages_into_out_queue(self, package):
         """
         """
-        yield self.out_put.put(PriorityItem(priority=self.env.now, item=package))
+        if isinstance(package, Package):
+            id_output_pip_line = package.next_pipeline
+            yield self.pipelines_dict[id_output_pip_line].put(package)
---
+++ cross/items.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+"""
+作者：Ted
+日期：2017-07-13
+说明：
+
+包裹 class
+货物 class
+Uld class
+
+"""
+import simpy
+import pandas as pd
+
+__all__ = ["Package", "Truck", "Uld", "SmallBag", "SmallPackage", "Pipeline"]
+
+
+class Package:
+    """包裹"""
+    def __init__(self,
+                 env: simpy.Environment,
+                 attr: pd.Series,
+                 item_id : str,
+                 path: tuple, ):
+
+        # 包裹的所有信息都在 attr
+        self.attr = attr
+        # id
+        self.item_id = item_id
+        # env
+        self.env = env
+        # for record
+        self.plan_path = path
+        # for popping
+        self.path = list(path)
+        # for time
+        self.time_records = []
+        # next pipeline_id
+        self.next_pipeline = ()
+        # record for package enter machine
+        self.package_record = dict(package_id=item_id)
+
+    def add_machine_id(self, machine_id):
+        self.package_record["machine_id"] = machine_id
+
+    def start_wait(self):
+        self.package_record["start_wait"] = self.env.now
+
+    def start_serve(self):
+        self.package_record["start_serve"] = self.env.now
+
+    def end_serve(self):
+        self.package_record["end_serve"] = self.env.now
+
+    def pop_mark(self):
+        """返回下一个pipeline id: (now_loc, next_loc)， 删去第一个节点，记录当前的时间点"""
+        if len(self.path) >= 2:
+            now_loc, next_loc = self.path[0: 2]
+        # 当 package 去到 reload（终分拣）， 终分拣的队列 id 只有一个值
+        elif len(self.path) == 1:
+            now_loc, next_loc = self.path[-1], None
+        else:
+            raise ValueError('The path have been empty!')
+        # remove the now_loc
+        pop_loc = self.path.pop(0)
+        self.time_records.append((pop_loc, self.env.now))
+        # 改变下一个 pipeline id
+        self.next_pipeline = now_loc, next_loc
+
+    def __str__(self):
+        display_dct = dict(self.attr)
+        return f"<package attr:{dict(display_dct)}, path: {self.plan_path}>"
+
+
+class SmallPackage(Package):
+    """小件包裹"""
+    def __str__(self):
+        display_dct = dict(self.attr)
+        return f"<SmallBag attr:{dict(display_dct)}, path: {self.plan_path}>"
+
+
+class SmallBag(Package):
+    """小件包"""
+    # todo
+    def __init__(self, env: simpy.Environment,
+                 attr: pd.Series,
+                 item_id : str,
+                 path: tuple,
+                 small_packages: pd.DataFrame):
+
+        super(SmallBag, self).__init__(env, attr, item_id, path)
+
+        self.store = small_packages
+        self.store_size = len(self.store)
+
+    def __str__(self):
+        display_dct = dict(self.attr)
+        return f"<SmallBag attr:{dict(display_dct)}, path: {self.plan_path}, store_size:{store_size}>"
+
+
+class Truck:
+    """货车"""
+    def __init__(self, env: simpy.Environment, item_id: str, come_time: int, truck_type: str, packages:pd.DataFrame):
+        """
+        :param truck_id: self explain
+        :param come_time: self explain
+        :param packages: a dataframe contain all packages
+        """
+        self.item_id = item_id
+        self.come_time = come_time
+        self.store = packages
+        self.store_size = len(self.store)
+        self.truck_type = truck_type
+        self.env = env
+
+    def __str__(self):
+        return f"<truck_id: {self.item_id}, come_time: {self.come_time}, store_size:{self.store_size}>"
+
+
+class Uld(Truck):
+    """航空箱"""
+    pass
+
+
+class Pipeline:
+
+    """传送带"""
+
+    def __init__(self,
+                 env: simpy.Environment,
+                 delay_time: float=0,
+                 pipeline_id: tuple=None,
+                 queue_id: str=None,
+                 machine_type: str=None,
+                 ):
+
+        self.env = env
+        self.delay = delay_time
+        self.queue = simpy.Store(env)
+        self.pipeline_id = pipeline_id
+        self.queue_id = queue_id
+        self.machine_type = machine_type
+        # 传送带上货物的计数
+        self.latency_counts = 0
+        self.latency_counts_time = []
+        # 机器等待区， 队列的计数
+        self.machine_waiting_counts_time = []
+
+        # 加入计数器
+        self.env.process(self.get_counts())
+
+    def get_counts(self):
+        """计数器"""
+        while True:
+
+            latency_dict = dict(pipeline_id=self.pipeline_id,
+                                timestamp=self.env.now,
+                                counts=self.latency_counts)
+
+            wait_dict = dict(pipeline_id=self.pipeline_id,
+                             timestamp=self.env.now,
+                             counts=len(self.queue.items))
+
+            self.latency_counts_time.append(latency_dict)
+            self.machine_waiting_counts_time.append(wait_dict)
+
+            yield self.env.timeout(1)
+
+    def latency(self, item: Package):
+        """模拟传送时间"""
+        self.latency_counts += 1
+        yield self.env.timeout(self.delay)
+        # 加入数据点
+        item.pop_mark()
+        item.add_machine_id()
+        self.queue.put(item)
+        self.latency_counts -= 1
+
+    def put(self, item: Package):
+        item.start_wait = self.env.now
+        self.env.process(self.latency(item))
+
+    def get(self):
+        return self.queue.get()
+
+    def __str__(self):
+        return f"<Pipeline: {self.pipeline_id}, delay: {self.delay}, package_counts: {self.latency_counts}>"
+
+if __name__ == '__main__':
+    pass
+
