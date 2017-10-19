aca2158a3907c7d0c8e4387f5766488bcf322ca1
chois lu <chois_lu@outlook.com>
2017-10-19 18:29:50 +0800

    update

--- cook_book_sim/decorator.py
+++ cook_book_sim/decorator.py
@@ -2,14 +2,16 @@ from functools import wraps
 import logging
 
 
-def logged(*, level_str=None, user: str = 'root', file_handal=None):
+
+def logged(*, level_str=None, user: str = 'root', file_handler=None):
     """
     Add logging to a function. level is the logging
     level, name is the logger name, and message is the
     log message. If name and message aren't specified,
     they default to the function's module and name.
     """
-    KYE_LEVEL = {'INFO': logging.INFO, 'ERRO': logging.ERROR,
+    KYE_LEVEL = {'INFO': logging.INFO,
+                 'ERRO': logging.ERROR,
                  'DEBUG': logging.DEBUG}
 
     if level_str is None:
@@ -22,11 +24,11 @@ def logged(*, level_str=None, user: str = 'root', file_handal=None):
 
     log_name = user
     logger = logging.getLogger(log_name)
-    format_ = '%(asctime)s %(name)s %(message)s'
+    format_ = '<%(asctime)s><%(name)s>: %(message)s'
     logging.basicConfig(format=format_, level=level)
 
-    if file_handal is not None:
-        fh = logging.FileHandler(file_handal)
+    if file_handler is not None and len(file_handler)>0:
+        fh = logging.FileHandler(file_handler)
         formatter = logging.Formatter(format_)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
@@ -48,11 +50,12 @@ def logged(*, level_str=None, user: str = 'root', file_handal=None):
 
 
 # Example use log
-@logged(level_str='INFO', user='sim', file_handal='test_log_ss.log')
+@logged(level_str='INFO', user='sim', file_handler='test_log_ss.log')
 def log_out(log=logging):
     loggers = log.info
-
     return loggers
+
+
 
 # if __name__ == '__main__':
 #     log = log_out()
